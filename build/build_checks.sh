e5e496d1c950e67122cd2ae74dffda7fdf48c8f6
Leif Madsen <leif@leifmadsen.com>
2020-12-01 16:35:52 -0500

    Revert "Bump build checks to using UBI8"
    This reverts commit ae3470422f81d577c58250b6728145226bd881b5.
    
    annocheck is not available on ubi8 base image. Using Fedora should be
    fine for some minor validation testing.

--- build/build_checks.sh
+++ build/build_checks.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-cp build/repos/* /etc/yum.repos.d/
 dnf install make gcc qpid-proton-c-devel annobin-annocheck rpm-build -y
 
 make 'CFLAGS=-O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fexceptions -fstack-protector-strong -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection' 'LDFLAGS=-Wl,-z,relro  -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld'
